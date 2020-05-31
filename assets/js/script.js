46803c4d6de68946e540ec4cc5ebd05d8708a5f8
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-31 20:57:21 +0100

    updated sign in home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
     
+    function fadeInOnLoad() {
+        $('.account_signin_title').animate({
+            opacity: '1',
+        }, function () {
+            $('#email_input').animate({
+                opacity: '1'
+            }, function () {
+                $('#password_input').animate({
+                    opacity: '1'
+                }, function () {
+                    $('#sign_up_button').animate({
+                        width: '60px'
+                    }, function() {
+                        $('#sign_up_button').animate({
+                            height: '30px'
+                        })
+                    })
+                })
+            })
+        })
+    }
+    window.onload = fadeInOnLoad;
 })
