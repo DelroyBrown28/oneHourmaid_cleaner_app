fd8dc82e9c7aea9c5048163daae7b7827af954ac
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-06-05 11:31:34 +0100

    calendar on cleaner account page now opens when clickin the calendar button

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,8 +90,27 @@ $(document).ready(function () {
         selector: "#myCalendar"
     })
 
-    $('.vanilla-calendar-date').click(function() {
+    $('.vanilla-calendar-date').click(function () {
         $(this).addClass('calnder_bg_color');
+    })
+
+
+    // SHOW CALENDAR BUTTON
+
+    $('#open_calendar_btn').click(function () {
+        $('.options_fade_wrap').fadeOut('fast', function () {
+            $('.calendar_wrap').fadeIn('fast', function () {
+                $('.close_button_wrap').fadeIn();
+            });
+        });
+    })
+
+    $('.close_button_wrap').click(function () {
+        $(this).fadeOut('fast', function () {
+            $('.calendar_wrap').fadeOut('fast', function () {
+                $('.options_fade_wrap').fadeIn();
+            })
+        })
     })
 
 
