e169aa805ca9991041d37fd692754bed62582d10
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-08-05 19:23:42 +0100

    Added validation to password input on register form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
 $(document).ready(function () {
 
+    // VALIDATION FOR REGISTER FORM
+
+    var passwordInput = document.getElementById('password_reg');
+    var number = document.getElementById('number');
+    var lowercase = document.getElementById('lowercase');
+    var uppercase = document.getElementById('uppercase');
+    var length = document.getElementById('length');
+
+    // Validate lowercase letters
+    passwordInput.onkeyup = function () {
+        var lowerCaseLetters = /[a-z]/g;
+        if (passwordInput.value.match(lowerCaseLetters)) {
+            lowercase.classList.remove('invalid');
+            lowercase.classList.add('valid');
+        } else {
+            lowercase.classList.remove('valid');
+            lowercase.classList.add('invalid');
+        }
+
+        // Validate capital letters
+
+        var capitalLetters = /[A-Z]/g;
+        if (passwordInput.value.match(capitalLetters)) {
+            uppercase.classList.remove('invalid');
+            uppercase.classList.add('valid');
+        } else {
+            uppercase.classList.remove('valid');
+            uppercase.classList.add('invalid');
+        }
+
+        // Validate numbers
+        var numbers = /[0-9]/g;
+        if (passwordInput.value.match(numbers)) {
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+        }
+
+        // Validate length
+        if (passwordInput.value.length >= 8) {
+            length.classList.remove("invalid");
+            length.classList.add("valid");
+        } else {
+            length.classList.remove("valid");
+            length.classList.add("invalid");
+        }
+
+    }
+
+
+
 
     function cleanerAppFadeIn() {
         $('#cleaners_avatar').animate({
